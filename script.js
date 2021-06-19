db9ea0846f69de634217aed606297a842dd68a69
maureensanchez99 <39846411+maureensanchez99@users.noreply.github.com>
2021-06-18 21:29:52 -0700

    qoom sync at Sat Jun 19 2021 04:29:52 GMT+0000 (Coordinated Universal Time)

--- script.js
+++ script.js
@@ -1,12 +1,2 @@
-// JavaScript for the homepage example index.html
-// Add a current year to the footer
-const footerYear = document.querySelector("#year");
-
-function getTime(){
-	const date = new Date(),
-		yyyy = date.getFullYear();
-		
-	footerYear.innerHTML = `${yyyy}`;
+function f(){
 }
-
-getTime();
