21c02feeb8d837d3446b69099249421029755acd
maureensanchez99 <39846411+maureensanchez99@users.noreply.github.com>
2021-06-20 07:12:35 -0700

    qoom sync at Sun Jun 20 2021 14:12:34 GMT+0000 (Coordinated Universal Time)

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
-function f(){
+window.logout = function() {
+
+	// TELLS OUR SERVER TO LOG THE USER OUT
+	fetch('/~/projects/Uplift/logout', { method: 'POST'});
+	
+	// TELLS OUR BROWSER TO SHOW A MESSAGE TO OUR USER
+	alert('Logged Out!');
+	
+	// REDIRECTS THE USER TO THE HOMEPAGE
+	location.href = '/~/projects/Uplift/index'
 }
